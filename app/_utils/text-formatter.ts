0fbab8be4e632bb8cae159187210ecf05ad245ff
zelton02 <zhiheng426@gmail.com>
2024-07-25 16:31:00 +0800

    Solve type error

--- app/_utils/text-formatter.ts
+++ app/_utils/text-formatter.ts
@@ -1,4 +1,8 @@
-const toTitleCase = (str: string) => {
+const toTitleCase = (str: string | undefined | null): string => {
+  if (typeof str !== 'string' || !str) {
+    return '';
+  }
+  
   return str.replace(/\w\S*/g, (txt) => {
     return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
   });
