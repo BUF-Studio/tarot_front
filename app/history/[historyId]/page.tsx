47a6a33604038aad652fc285975e9947a7894a83
Ng Gih Ming <n.gihming@yahoo.com>
2024-08-23 18:47:15 +0800

    feat: history page done

--- app/history/[historyId]/page.tsx
+++ app/history/[historyId]/page.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import styles from "../history.module.scss";
 import Image from "next/image";
-import TextField from "@mui/material/TextField";
-import Link from "next/link";
-import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
-import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
+import Chip from "@mui/material/Chip";
 
 import { toTitleCase } from "../../_utils/text-formatter";
 import { Session } from "../session-interface";
-
 export default function HistoryDetails() {
-
-  // query the session to frontend from repository
   const currentSession: Session = {
     id: "1",
     question: "What is the meaning of life?",
@@ -44,35 +35,84 @@ export default function HistoryDetails() {
 
   return (
     <div className={styles.historyContainer}>
-
       <Stack
-        width="100%"
+        maxWidth="md"
+        sx={{ width: "100%", mt: 2, mb: 4 }}
+        className={styles.header}
+      >
+        <Stack
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="baseline"
+          maxWidth="md"
+          sx={{ width: "100%" }}
+        >
+          <h1 className={`title-large ${styles.title}`}>
+            {currentSession.question}
+          </h1>
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          maxWidth="md"
+          sx={{ width: "100%", mt: 1 }}
+          alignItems="baseline"
+        >
+          <h1 className={`body-large ${styles.title}`}>
+            {currentSession.session_created.toUTCString()}
+          </h1>
+          <Chip label={toTitleCase(currentSession.stage)} />
+        </Stack>
+      </Stack>
+      <Stack
         direction="row"
         justifyContent="space-between"
-        alignItems="center"
         maxWidth="md"
+        sx={{ width: "100%", mt: 1, mb: 4 }}
+        alignItems="baseline"
+        className={styles.summary}
       >
-        <Image
-          src="/logo.png"
-          alt="Logo"
-          width={64}
-          height={64}
-          className={styles.logo}
-        />
-        
+        <h1 className={`headline-medium`} style={{ fontWeight: "bold" }}>
+          {currentSession.summary}
+        </h1>
       </Stack>
-      <Container maxWidth="sm">
-        <TextField
-          fullWidth
-          id="search"
-          label="Search History"
-          name="search"
-          autoComplete="search"
-        />
-        <Divider sx={{ my: 2 }} />
-      </Container>
+      <div className={styles.cardContainer} style={{ padding: 0 }}>
+        {currentSession.cards.map((card, index) => (
+          <Card
+            key={index}
+            card={card}
+            isCurrentCard={index == currentSession.current_card}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 
-const card = {};
+const Card = ({
+  card,
+  isCurrentCard,
+}: {
+  card: { position: string; description: string };
+  isCurrentCard: boolean;
+}) => {
+  return (
+    <div className={styles.flipCard}>
+      <div className={styles.flipCardInner}>
+        <div className={styles.flipCardFront}>
+          <Image src="/logo-188-272.png" alt="Card" width={188} height={272} />
+        </div>
+        <div className={styles.flipCardBack}>
+          <div
+            className={`${styles.flipCardInnerBack} ${
+              isCurrentCard ? styles.currentCard : styles.notCurrentCard
+            }`}
+          >
+            <h1 className={`title-medium`}>{card.position}</h1>
+            <p className={`body-medium`}>{card.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
